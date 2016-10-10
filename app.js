847864f4ab3189206eab7ddc06c9c65f180eb7bf
StevenShamoo <steven_shamoo@yahoo.com>
2016-10-10 11:08:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,13 @@ var bodyparser = require('body-parser');
 var mongodb = require('mongodb');
 var monk = require('monk');
 
-// This is kind of cheating as I'm only checking to see if a PORT is
-//  provided to us in the node env, but it should illustrate the point...
+
 if (process.env.PORT) {
   var db = monk('localhost:27017/todoforu');
 } else {
   var db = monk('localhost/todoforu');
 }
-// When I used heroku, I believe I put the heroku url in an env variable
-//  for the app on heroku, so I would have checked for that (and used
-//  it if I found it) instead of checking the PORT. However, you'll find
-//  that as you build out your apps, you'll have more and more setup that
-//  relies on the environment in which he app is running. For that reason,
-//  it is probably worth checking out an environment management package, or
-//  setting up a config json file (that is in the gitignore) and passing it
-//  to some configEnv function that runs when you start up your server.
+
 
 var index = require('./routes/index');
 
